24676bb4e08174d627fc73aadcbc045026406c37
Matheus Costa <pedrohenrique.familia@gmail.com>
2025-11-09 22:34:55 -0300

    removido arquivo de teste

--- Controller/teste_controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h1>SUCESSO!</h1>";
-echo "<p style='font-size: 20px;'>Você conseguiu chegar ao Controller!</p>";
-echo "<p>Isso prova que o caminho <code>../Controller/</code> está <strong>CORRETO</strong>.</p>";
-?>
