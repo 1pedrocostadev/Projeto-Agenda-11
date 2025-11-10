7005740f6661a8b519d0f3f25000b8bc85a79336
Matheus Costa <pedrohenrique.familia@gmail.com>
2025-11-09 22:31:22 -0300

    add
    adicionando nova pasta controller e a imagem do projeto

---
+++ Controller/OutrasFormacoesController.php
@@ -0,0 +1,35 @@
+<?php
+if(!isset($_SESSION))
+{
+    session_start();
+}
+class OutrasFormacoesController{
+
+    // Método para o botão btnAddOF
+    public function inserir($inicio, $fim, $descricao, $idusuario) {
+        require_once 'Model/OutrasFormacoes.php';
+        $of = new OutrasFormacoes();
+        $of->setInicio($inicio);
+        $of->setFim($fim);
+        $of->setDescricao($descricao);
+        $of->setIdUsuario($idusuario);
+        $r = $of->inserirBD();
+        return $r;
+    }
+
+    // Método para o botão btnExcluirOF
+    public function remover($id) {
+        require_once 'Model/OutrasFormacoes.php';
+        $of = new OutrasFormacoes();
+        $r = $of->excluirBD($id);
+        return $r;
+    }
+
+    public function gerarLista($idusuario)
+    {
+        require_once 'Model/OutrasFormacoes.php';
+        $of = new OutrasFormacoes();
+        return $results = $of->listaFormacoes($idusuario);
+    }
+}
+?>
