9996f8411350692cabdadb4a3131769bff2e8499
Matheus Costa <pedrohenrique.familia@gmail.com>
2025-11-09 22:37:31 -0300

    mais mudanças na pasta view

--- View/ADMPrincipal.php
+++ View/ADMPrincipal.php
@@ -1,12 +1,5 @@
-<?php
-// CORREÇÃO: session_start() deve vir ANTES de qualquer HTML
-if (!isset($_SESSION)) {
-    session_start();
-}
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,44 +8,34 @@ if (!isset($_SESSION)) {
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Painel de Administração</title>
-    <style>
-        body, h1, h2, h3, h4, h5, h6 {
-            font-family: "Montserrat", sans-serif;
-        }
-    </style>
+    <style> body, h1, h2, h3, h4, h5, h6 { font-family: "Montserrat", sans-serif} </style>
 </head>
-
 <body class="w3-light-grey">
-
+    <?php
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    ?>
     <div class="w3-padding-large" id="main">
-
-        <header class="w3-container w3-padding-32 w3-center " id="home">
+        <header class="w3-container w3-padding-32 w3-center" id="home">
             <br>
             <h1 class="w3-text-white w3-panel w3-cyan w3-round-large"> ADMINISTRAÇÃO </h1>
-            <h1 class="w3-text-white w3-panel w3-cyan w3-round-large"> SISTEMA DE CURRICULOS </h1>
+            <h1 class="w3-text-white w3-panel w3-cyan w3-round-large"> SISTEMA DE CURRÍCULOS </h1>
         </header>
 
-        <form action="../Controller/navegacao.php" method="post" class="w3-container w3-light-grey w3-text-blue w3-margin w3-center" style="">
-            <input type="hidden" name="nome_form" value="frmLoginADM" />
+        <form action="index.php" method="post" class="w3-container w3-light-grey w3-text-blue w3-margin w3-center" style="">
             
             <button name="btnListarCadastrados" class="w3-button w3-margin w3-blue w3-cell w3-round-large" style="">
                 <br> <i class="fa fa-address-book-o w3-xxlarge"></i><br>
                 <p class="w3-xlarge">Usuários<br> Cadastrados</p>
             </button>
-
-            <div class="w3-row w3-section">
-                <div class="w3-half" style="">
-                    <button name="btnLogin" class="w3-button w3-block w3-margin w3-blue w3-cell w3-round-large" style="width: 90%;">Entrar</button>
-                </div>
-                <div class="w3-half">
-                    <button name="btnPrimeiroAcesso" class="w3-button w3-block w3-margin w3-blue w3-cell w3-round-large" style="width: 90%;">Primeiro Acesso?</button>
-                </div>
-                <div class="w3-center" style="">
-                    <button name="btnADM" class=" w3-block w3-margin w3-blue w3-cell w3-round-large" style="width: 90%;">Login como Administrador</button>
-                </div>
-            </div>
             
+            <button name="btnListarADM" class="w3-button w3-margin w3-blue w3-cell w3-round-large" style="">
+                <br> <i class="fa fa-user-secret w3-xxlarge"></i><br>
+                <p class="w3-xlarge">Listar<br> Administradores</p>
+            </button>
         </form>
-
-    </div> </body>
+    </div>
+</body>
 </html>
--- View/ADMVisualizarCadastro.php
+++ View/ADMVisualizarCadastro.php
@@ -1,150 +1,151 @@
-<?php
-if (!isset($_SESSION)) {
-    session_start();
-}
-
-include_once '../Controller/UsuarioController.php';
-include_once '../Controller/FormacaoAcadController.php';
-include_once '../Controller/ExperienciaProfissionalController.php';
-
-$idUsuario = $_SESSION['idUserVis'];
-
-$uController = new UsuarioController();
-$usuario = $uController->buscarPorId($idUsuario);
-
-$fController = new FormacaoAcadController();
-$listaFormacao = $fController->gerarLista($idUsuario);
-
-$eController = new ExperienciaProfissionalController();
-$listaExp = $eController->gerarLista($idUsuario);
-
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Visualizar Cadastro</title>
-    <style>
-        body, h1, h2, h3, h4, h5, h6 { font-family: "Montserrat", sans-serif }
-    </style>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<title>Visualizar Cadastro</title>
+<style> body, h1, h2, h3, h4, h5, h6 { font-family: "Montserrat", sans-serif } </style>
 </head>
-
 <body class="w3-light-grey">
-    <div class="w3-padding-large" id="main"> 
+    <?php
+    include_once 'Controller/UsuarioController.php';
+    include_once 'Controller/FormacaoAcadController.php';
+    include_once 'Controller/ExperienciaProfissionalController.php';
+    include_once 'Controller/OutrasFormacoesController.php'; 
+    
+    if(!isset($_SESSION))
+    {
+        session_start();
+    }
+    
+    $idVis = $_SESSION['idUsuarioVisualizar'];
+    $uController = new UsuarioController();
+    $usuarioVis = $uController->visualizarUsuario($idVis);
+    ?>
 
-        <header class="w3-container w3-padding-32 w3-center ">
-            <h1 class="w3-text-white w3-panel w3-cyan w3-round-large">
-                Visualização de Cadastro
-            </h1>
-        </header>
+<div class="w3-padding-large" id="main">
+<center> 
+<header class="w3-container w3-padding-32 w3-center" id="home">
+    <h1 class="w3-text-white w3-panel w3-cyan w3-round-large">Currículo de <?php echo $usuarioVis->getNome();?></h1>
+</header>
+</center> 
 
-        <div class="w3-content w3-text-grey w3-card w3-container" style="padding: 20px;">
-            <h2 class="w3-text-cyan"><i class="fa fa-user"></i> Dados Pessoais</h2>
-            
-            <div class="w3-row-padding">
-                <div class="w3-half w3-margin-bottom">
-                    <label>Nome Completo</label>
-                    <input class="w3-input w3-border w3-light-grey" type="text" value="<?php echo $usuario->getNome(); ?>" disabled>
-                </div>
-                <div class="w3-half">
-                    <label>CPF</label>
-                    <input class="w3-input w3-border w3-light-grey" type="text" value="<?php echo $usuario->getCPF(); ?>" disabled>
-                </div>
-            </div>
-            <div class="w3-row-padding">
-                <div class="w3-half">
-                    <label>Email</label>
-                    <input class="w3-input w3-border w3-light-grey" type="text" value="<?php echo $usuario->getEmail(); ?>" disabled>
-                </div>
-                <div class="w3-half">
-                    <label>Data de Nascimento</label>
-                    <?php
-                        $dataNasc = $usuario->getDataNascimento();
-                        if($dataNasc) {
-                            $dataNasc = date("d/m/Y", strtotime($dataNasc));
-                        }
-                    ?>
-                    <input class="w3-input w3-border w3-light-grey" type="text" value="<?php echo $dataNasc; ?>" disabled>
-                </div>
-            </div>
-        </div>
+<div class="w3-content w3-text-grey" id="dPessoais">
+<h2 class="w3-text-cyan">Dados Pessoais</h2>
+</div>
+<center>
+<div class=" w3-row w3-light-grey w3-text-blue w3-margin w3-padding" style="width:70%;">
+    <p><b>NOME:</b> <?php echo $usuarioVis->getNome();?></p>
+    <p><b>CPF:</b> <?php echo $usuarioVis->getCPF();?></p>
+    <p><b>EMAIL:</b> <?php echo $usuarioVis->getEmail();?></p>
+    <p><b>DATA DE NASCIMENTO:</b> <?php echo $usuarioVis->getDataNascimento();?></p>
+</div>
+</center> 
 
-        <div class="w3-padding-32 w3-content w3-text-grey">
-            <h2 class="w3-text-cyan"><i class="fa fa-mortar-board"></i> Formação Acadêmica</h2>
-            <div class="w3-container">
-                <table class="w3-table-all w3-centered">
-                    <thead>
-                        <tr class="w3-center w3-blue">
-                            <th>Início</th>
-                            <th>Fim</th>
-                            <th>Descrição</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        if ($listaFormacao != null && $listaFormacao->num_rows > 0) {
-                            while ($row = $listaFormacao->fetch_object()) {
-                                echo '<tr>';
-                                echo '<td>' . date("d/m/Y", strtotime($row->inicio)) . '</td>';
-                                echo '<td>' . date("d/m/Y", strtotime($row->fim)) . '</td>';
-                                echo '<td>' . $row->descricao . '</td>';
-                                echo '</tr>';
-                            }
-                        } else {
-                            echo '<tr><td colspan="3">Nenhuma formação cadastrada.</td></tr>';
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+<div class="w3-padding-128 w3-content w3-text-grey" id="formacao">
+<h2 class="w3-text-cyan">Formação Acadêmica</h2>
+</div>
+<center>
+<div class="w3-container" style="width:70%;">
+<table class="w3-table-all w3-centered">
+ <thead> 
+ <tr class="w3-center w3-blue">
+ <th>Início</th>
+ <th>Fim</th>
+ <th>Descrição</th>
+ </tr>
+ </thead> 
+ <?php
+ $fCon = new FormacaoAcadController();
+ $results = $fCon->gerarLista($usuarioVis->getID());
+ if($results != null)
+ while($row = $results->fetch_object()) {
+ echo '<tr>';
+ echo '<td style="width: 20%;">'.$row->inicio.'</td>';
+ echo '<td style="width: 20%;">'.$row->fim.'</td>';
+ echo '<td style="width: 60%;">'.$row->descricao.'</td>';
+ echo '</tr>';
+ }
+ ?>
+ </table>
+</div>
+</center> 
 
-        <div class="w3-padding-32 w3-content w3-text-grey">
-            <h2 class="w3-text-cyan"><i class="fa fa-briefcase"></i> Experiência Profissional</h2>
-            <div class="w3-container">
-                <table class="w3-table-all w3-centered">
-                    <thead>
-                        <tr class="w3-center w3-blue">
-                            <th>Início</th>
-                            <th>Fim</th>
-                            <th>Empresa</th>
-                            <th>Descrição</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        if ($listaExp != null && $listaExp->num_rows > 0) {
-                            while ($row = $listaExp->fetch_object()) {
-                                echo '<tr>';
-                                echo '<td>' . date("d/m/Y", strtotime($row->inicio)) . '</td>';
-                                echo '<td>' . date("d/m/Y", strtotime($row->fim)) . '</td>';
-                                echo '<td>' . $row->empresa . '</td>';
-                                echo '<td>' . $row->descricao . '</td>';
-                                echo '</tr>';
-                            }
-                        } else {
-                             echo '<tr><td colspan="4">Nenhuma experiência cadastrada.</td></tr>';
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        
-        <div class="w3-padding-32 w3-content">
-            <form action="/Controller/navegacao.php" method="post" class="w3-container w3-margin w3-center">
-                <button name="btnListarCadastrados" class="w3-button w3-block w3-blue w3-cell w3-round-large" style="width: 30%;">
-                    <i class="fa fa-arrow-left"></i> Voltar para Lista
+<div class="w3-padding-128 w3-content w3-text-grey" id="eProfissional">
+<h2 class="w3-text-cyan">Experiência Profissional</h2>
+</div>
+<center> 
+<div class="w3-container" style="width:70%;">
+<table class="w3-table-all w3-centered">
+ <thead> 
+ <tr class="w3-center w3-blue">
+ <th>Início</th>
+ <th>Fim</th>
+ <th>Empresa</th>
+ <th>Descrição</th>
+ </tr>
+ </thead> 
+ <?php
+ $ePro = new ExperienciaProfissionalController();
+ $results = $ePro->gerarLista($usuarioVis->getID());
+ if($results != null)
+ while($row = $results->fetch_object()) {
+ echo '<tr>';
+ echo '<td style="width: 20%;">'.$row->inicio.'</td>';
+ echo '<td style="width: 20%;">'.$row->fim.'</td>';
+ echo '<td style="width: 30%;">'.$row->empresa.'</td>';
+ echo '<td style="width: 30%;">'.$row->descricao.'</td>';
+ echo '</tr>';
+ }
+ ?>
+ </table>
+</div>
+</center> 
+
+<div class="w3-padding-128 w3-content w3-text-grey" id="oFormacoes">
+<h2 class="w3-text-cyan">Outras Formações</h2>
+</div>
+<center> 
+<div class="w3-container" style="width:70%;">
+<table class="w3-table-all w3-centered">
+<thead> 
+<tr class="w3-center w3-blue">
+<th>Início</th>
+<th>Fim</th>
+<th>Descrição</th>
+</tr>
+</thead> 
+ <?php
+ $oCon = new OutrasFormacoesController();
+ $results = $oCon->gerarLista($usuarioVis->getID());
+ if($results != null)
+ while($row = $results->fetch_object()) {
+ echo '<tr>';
+ echo '<td style="width: 20%;">'.$row->inicio.'</td>';
+ echo '<td style="width: 20%;">'.$row->fim.'</td>';
+ echo '<td style="width: 60%;">'.$row->descricao.'</td>';
+ echo '</tr>';
+ }
+ ?>
+</table>
+</div>
+
+<div class="w3-padding-128 w3-content w3-text-grey">
+    <form action="index.php" method="post" class="w3-container w3-light-grey w3-text-blue w3-margin w3-center" style="width: 30%;">
+        <div class="w3-row w3-section">
+            <div>
+                <button name="btnVoltarADMLista" class="w3-button w3-block w3-margin w3-blue w3-cell w3-round-large" style="width: 90%;"> 
+                    Voltar para Lista
                 </button>
-            </form>
+            </div>
         </div>
-
-    </div>
+    </form>
+</div>
+</center> 
+</div> 
 </body>
 </html>
